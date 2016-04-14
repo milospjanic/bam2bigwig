c50ab1d88abaf17381c0bc93dd82d5f6ad930ab6
Milos Pjanic <pjanic.milos@gmail.com>
2016-04-14 09:34:39 -0700

    Update bam2bigwig.sh

--- bam2bigwig.sh
+++ bam2bigwig.sh
@@ -13,21 +13,18 @@ chmod 775 fetchChromSizes
 ./fetchChromSizes hg19 > hg19.chrom.sizes
 
 #create plus and minus strand bedgraph
-awk '{if($6=="+") print}' accepted_hits.bed | sort -k1,1 | ./bedItemOverlapCount hg19 -chromSize=hg19.chrom.sizes stdin | sort -k1,1 -k2,2n > accepted_hits.plus.bedGraph
-awk '{if($6=="-") print}' accepted_hits.bed | sort -k1,1 | ./bedItemOverlapCount hg19 -chromSize=hg19.chrom.sizes stdin | sort -k1,1 -k2,2n | awk '{OFS="\t"; print $1,$2,$3,"-"$4}' > accepted_hits.minus.bedGraph
+cat accepted_hits.bed | sort -k1,1 | ./bedItemOverlapCount hg19 -chromSize=hg19.chrom.sizes stdin | sort -k1,1 -k2,2n > accepted_hits.bedGraph
 
 #download bedGraphToBigWig
 wget http://hgdownload.soe.ucsc.edu/admin/exe/linux.x86_64/bedGraphToBigWig
 chmod 755 bedGraphToBigWig
 
 #create plus and minus strand bigwig
-./bedGraphToBigWig accepted_hits.plus.bedGraph hg19.chrom.sizes $1.plus.bw
-./bedGraphToBigWig accepted_hits.minus.bedGraph hg19.chrom.sizes $1.minus.bw
+./bedGraphToBigWig accepted_hits.bedGraph hg19.chrom.sizes $1.bw
 
 #removing intermediery files
 rm accepted_hits.bed
-rm accepted_hits.plus.bedGraph
-rm accepted_hits.minus.bedGraph
+rm accepted_hits.bedGraph
 rm bedItemOverlapCount
 rm bedGraphToBigWig
 rm fetchChromSizes
